b34f85e3a3bbae03beb39781025213005a504ce7
Lucas <lucas.girardin@outlook.com>
2014-10-24 00:04:14 +0200

    init

---
+++ src/main/Main.java
@@ -0,0 +1,52 @@
+package main;
+
+import java.util.List;
+
+import dijkstra_router.GenericEdge;
+import dijkstra_router.GenericNode;
+import dijkstra_router.Graph;
+import algo.graph.interfaces.IEdge;
+import algo.graph.interfaces.INode;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Graph<String, Object> graph = new Graph<>();
+
+		// Build node.
+		GenericNode<String, Object> paris = new GenericNode<>("Paris");
+		GenericNode<String, Object> lyon = new GenericNode<>("Lyon");
+		GenericNode<String, Object> grenoble = new GenericNode<>("Grenoble");
+		GenericNode<String, Object> valence = new GenericNode<>("Valence");
+		GenericNode<String, Object> gap = new GenericNode<>("Gap");
+		GenericNode<String, Object> marseille = new GenericNode<>("Marseille");
+
+		graph.registerNode(paris);
+		graph.registerNode(lyon);
+		graph.registerNode(grenoble);
+		graph.registerNode(valence);
+		graph.registerNode(gap);
+		graph.registerNode(marseille);
+		
+		// Build edges
+		new GenericEdge(paris, lyon, 400);
+		new GenericEdge(lyon, grenoble, 100);
+		new GenericEdge(lyon, valence, 100);
+		new GenericEdge(grenoble, valence, 100);
+		new GenericEdge(grenoble, gap, 100);
+		new GenericEdge(gap, marseille, 150);
+		new GenericEdge(valence, marseille, 250);
+		
+		INode<String, Object> node = graph.getNode("Grenoble");
+		
+		List<IEdge> edges = node.getEdges();
+		for (IEdge edge : edges)
+			System.out.println(edge.getOther(node) + " -> " + edge.getAttribute("cost")); 
+		   // Lyon -> 100
+		   // Valence -> 100
+		   // Gap > 100
+	}
+
+}
